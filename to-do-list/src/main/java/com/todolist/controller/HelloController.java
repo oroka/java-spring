8b7c8608354cae8658cd87b342539eec3ea1647c
kohei miyamoto <kmtary13@gmail.com>
2019-03-15 10:32:25 +0900

    add a little security

--- to-do-list/src/main/java/com/todolist/controller/HelloController.java
+++ to-do-list/src/main/java/com/todolist/controller/HelloController.java
@@ -1,20 +1,33 @@
 package com.todolist.controller;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.todolist.domain.ItemDto;
+import com.todolist.security.ToDoListLoginUser;
 
 @Controller
+@RequestMapping
 public class HelloController {
 	
 	@GetMapping(path = {"", "hello"})
-    public String greeting(Model model) {
+    public String greeting(Model model, Principal principal) {
+		
+		System.out.println("principal : " + principal);
+		if(principal != null) {
+			Authentication auth = (Authentication)principal;
+			ToDoListLoginUser user = (ToDoListLoginUser)auth.getPrincipal();
+			model.addAttribute("userLoginFlag", user.getUser());
+		}
+		
         model.addAttribute("message", "HELLO WORLD");
         model.addAttribute("date", LocalDateTime.of(2018, 4, 13, 11, 12, 13));
         List<ItemDto> items = Arrays.asList(
